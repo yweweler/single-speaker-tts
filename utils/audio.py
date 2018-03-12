ee1e88693cc0cdd9870d586d17770dc3a8d55dad
Yves-Noel Weweler <y.weweler@gmail.com>
2018-03-12 11:37:42 +0100

    Add documentation
    * Documentation for utils/audio.py

--- utils/audio.py
+++ utils/audio.py
@@ -18,30 +18,82 @@ AUDIO_FLOAT_EPS = 1e-7
 
 
 def magnitude_to_decibel(mag):
-    # TODO: Add documentation.
-    # TODO: Add decibel_to_magnitude function.
-    mag_db = 20.0 * np.log10(np.maximum(1e-5, mag))
+    """
+    Convert a magnitude spectrogram to decibel (dB).
+    Decibel values will be in the range [-100 dB, +inf dB]
 
-    return mag_db
+    :param mag: np.ndarray
+         Magnitude spectrum.
+
+    :return: np.ndarray
+        Magnitude spectrum in decibel representation.
+
+        20 * log10(max(1e-5, abs(mag)))
+    """
+    # Decibel values are bounded to a lower level of -100 dB.
+    # 20 * log10(1e-5) = -100 dB
+    #
+    # There is no upper bound for the decibel values.
+    # The 0 dB level is reached in case of magnitudes with value 1.0.
+    # 20 * log10( 1.0) = 0 dB
+
+    return 20.0 * np.log10(np.maximum(1e-5, mag))
 
 
 def decibel_to_magnitude(mag_db):
-    # TODO: Add documentation.
+    """
+    Convert a magnitude spectrogram in decibel (dB) representation back to a raw magnitude spectrogram.
+
+    :param mag_db: np.ndarray
+        Magnitude spectrum in decibel representation.
+
+    :return:
+        Magnitude spectrum.
+
+        power(10, mag_db / 20)
+    """
+    # TODO: Limit passed mag_db to [-100 dB, +inf dB] before conversion?
     mag = np.power(10.0, mag_db / 20.0)
 
     return mag
 
 
-def normalize_decibel(mag_db, ref_db, max_db):
-    # TODO: Add documentation.
-    mag_db = np.clip((mag_db - ref_db + max_db) / max_db, 1e-8, 1.0)
+def normalize_decibel(db, ref_db, max_db):
+    """
+    Normalize decibel (dB) values to the range [0.0, 1.0].
 
-    return mag_db
+    :param db:
+        Data in decibel representation (dB).
+
+    :param ref_db:
+        Signal reference in decibel (dB).
+
+    :param max_db:
+        Signal maximum in decibel (dB).
+
+    :return:
+        clip((mag_db - ref_db + max_db) / max_db, 0.0, 1.0)
+    """
+    return np.clip((db - ref_db + max_db) / max_db, 0.0, 1.0)
 
 
-def inv_normalize_decibel(db, ref_db, max_db):
-    # TODO: Add documentation.
-    return (np.clip(db, 0.0, 1.0) * max_db) + ref_db - max_db
+def inv_normalize_decibel(norm_db, ref_db, max_db):
+    """
+    Convert normalized decibel (dB) values from the range [0.0, 1.0] back to decibel.
+
+    :param norm_db:
+        Normalized decibel values in the range [0.0, 1.0].
+
+    :param ref_db:
+        Signal reference in decibel (dB) used for normalization.
+
+    :param max_db:
+        Signal maximum in decibel (dB) used for normalization.
+
+    :return:
+        (clip(norm_db, 0.0, 1.0) * max_db) + ref_db - max_db
+    """
+    return (np.clip(norm_db, 0.0, 1.0) * max_db) + ref_db - max_db
 
 
 def load_wav(wav_path, sampling_rate=hparams.sampling_rate, offset=0.0, duration=None):
