0faa935e367caf95d59e33a0a69fec7f22e253c0
Yves-Noel Weweler <y.weweler@gmail.com>
2018-03-13 14:06:53 +0100

    Cleanup of the sandbox.py

--- sandbox.py
+++ sandbox.py
@@ -1,23 +1,19 @@
 import librosa
-import matplotlib.pyplot as plt
 import numpy as np
 import pysptk
-import sklearn
-from librosa import display
 
 from hparams import hparams
-from utils.features import linear_scale_spectrogram, mel_scale_spectrogram, calculate_mfccs, calculate_mceps
 from utils.conversion import ms_to_samples
-from utils.io import load_wav, save_wav
-from utils.visualization import plot_spectrogram, plot_feature_frames, plot_waveform
-from utils.synthesis import spectrogram_to_wav
+from utils.features import linear_scale_spectrogram, mel_scale_spectrogram, calculate_mfccs, calculate_mceps
+from utils.io import load_wav
+from utils.visualization import plot_spectrogram, plot_feature_frames
 
 
-def resynth_wav_using_mcep(wav, n_fft, hop_len, n_mceps, mcep_alpha):
+def resynth_wav_using_mcep(wav, hop_len, n_mceps, mcep_alpha):
     from pysptk.synthesis import MLSADF, Synthesizer
 
     # Calculate mceps.
-    mceps, mc = calculate_mceps(wav, n_fft=n_fft, hop_length=hop_len,
+    mceps, mc = calculate_mceps(wav, n_fft=hparams.n_fft, hop_length=hop_len,
                                 n_mceps=n_mceps, alpha=mcep_alpha)
 
     # Calculate source excitation.
@@ -41,93 +37,81 @@ def resynth_wav_using_mcep(wav, n_fft, hop_len, n_mceps, mcep_alpha):
     synthesizer = Synthesizer(MLSADF(order=n_mceps, alpha=mcep_alpha), hop_len)
     wav_synth = synthesizer.synthesis(source_excitation, b)
 
+    # save_wav('/tmp/synthesized.wav', wav_synth, sr, norm=True)
+    # plot_feature_frames(mc.T, sampling_rate=sr, hop_length=hop_len, title='MCEP')
+
+    # plot_spectrogram(mc,
+    #                  sampling_rate=sr,
+    #                  hop_length=hop_len,
+    #                  fmin=hparams.mel_fmin,
+    #                  fmax=hparams.mel_fmax,
+    #                  y_axis='linear',
+    #                  title='Spectral envelope estimate from mel-cepstrum')
+
     return mc, wav_synth
 
 
+def calculate_mfccs_and_deltas(wav, hop_len, win_len):
+    mel_spec = mel_scale_spectrogram(wav,
+                                     n_fft=hparams.n_fft,
+                                     sampling_rate=hparams.sampling_rate,
+                                     hop_length=hop_len,
+                                     win_length=win_len,
+                                     n_mels=hparams.n_mels,
+                                     fmin=hparams.mel_fmin,
+                                     fmax=hparams.mel_fmax,
+                                     power=2)
+
+    # Convert the Mel-scaled spectrogram to its decibel representation.
+    mel_spec_db = librosa.power_to_db(mel_spec, ref=np.max)
+
+    # Calculate mfcc features.
+    mfccs = calculate_mfccs(mel_spec_db, sr, hparams.n_mfcc)
+
+    plot_spectrogram(mel_spec_db,
+                     sampling_rate=hparams.sampling_rate,
+                     hop_length=hop_len,
+                     fmin=hparams.mel_fmin,
+                     fmax=hparams.mel_fmax,
+                     y_axis='mel',
+                     title='Mel-scale power spectrogram')
+
+    # # Calculate mfcc delta features.
+    mfccs_d1 = librosa.feature.delta(mfccs, width=3, order=1)
+    # Calculate mfcc delta-delta features.
+    mfccs_d2 = librosa.feature.delta(mfccs, width=3, order=2)
+
+    features = np.vstack((mfccs, mfccs_d1, mfccs_d2))
+
+    plot_feature_frames(mfccs, sampling_rate=sr, hop_length=hop_len, title='MFCC')
+    plot_feature_frames(mfccs_d1, sampling_rate=sr, hop_length=hop_len, title=r'MFCC-$\Delta$')
+    plot_feature_frames(mfccs_d2, sampling_rate=sr, hop_length=hop_len, title=r'MFCC-$\Delta^2$')
+
+
+def calculate_linear_spec(wav, hop_len, win_len):
+    linear_spec = linear_scale_spectrogram(wav,
+                                           n_fft=hparams.n_fft,
+                                           hop_length=hop_len,
+                                           win_length=win_len)
+
+    # Convert the linear-scaled spectrogram to its decibel representation.
+    linear_spec_db = librosa.amplitude_to_db(linear_spec, ref=np.max)
+
+    plot_spectrogram(linear_spec_db,
+                     sampling_rate=hparams.sampling_rate,
+                     hop_length=hop_len,
+                     fmin=hparams.mel_fmin,
+                     fmax=hparams.mel_fmax,
+                     y_axis='linear',
+                     title='Linear-scale power spectrogram')
+
+
 wav_path = '/home/yves-noel/documents/master/projects/datasets/timit/TIMIT/TRAIN/DR1/FCJF0/SA1.WAV'
-n_fft = hparams.n_fft
-
-wav, sr = load_wav(wav_path, sampling_rate=None)
-print("min(wav) = {}".format(min(wav)))
-print("max(wav) = {}".format(max(wav)))
-print("type(wav) = {}".format(wav.dtype))
-
-print("len(wav) = {}s".format(len(wav) / sr))
+wav, sr = load_wav(wav_path)
 
 win_len = ms_to_samples(hparams.win_len, sampling_rate=sr)
 hop_len = ms_to_samples(hparams.win_hop, sampling_rate=sr)
 
-linear_spec = linear_scale_spectrogram(wav,
-                                       n_fft,
-                                       hop_length=hop_len,
-                                       win_length=win_len)
-
-mel_spec = mel_scale_spectrogram(wav, n_fft, sampling_rate=sr,
-                                 hop_length=hop_len,
-                                 win_length=win_len,
-                                 n_mels=hparams.n_mels,
-                                 fmin=hparams.mel_fmin,
-                                 fmax=hparams.mel_fmax,
-                                 power=2)
-
-# Convert the linear-scaled power spectrogram to its decibel representation.
-linear_spec = linear_spec ** 2
-linear_spec_db = librosa.power_to_db(linear_spec, ref=np.max)
-
-# Convert the Mel-scaled power spectrogram to its decibel representation.
-mel_spec_db = librosa.power_to_db(mel_spec, ref=np.max)
-
-# Calculate mfcc features.
-mfccs = calculate_mfccs(mel_spec_db, sr, hparams.n_mfcc)
-
-# wav to mcep to wav resynthesis.
-# mc, wav_synth = resynth_wav_using_mcep(wav, hparams.n_fft, hop_len, 39, 0.42)
-# save_wav('/tmp/original.wav', wav, sr, norm=True)
-# save_wav('/tmp/synthesized.wav', wav_synth, sr, norm=True)
-# plot_feature_frames(mc.T, sampling_rate=sr, hop_length=hop_len, title='MCEP')
-# plot_waveform(wav, sampling_rate=sr)
-# plot_waveform(wav_synth, sampling_rate=sr)
-# exit()
-
-wav_synth = spectrogram_to_wav(np.sqrt(linear_spec),
-                               win_len,
-                               hop_len,
-                               hparams.n_fft,
-                               50)
-save_wav('/tmp/synthesized_griffin.wav', wav_synth, sr, norm=True)
-exit()
-
-plot_spectrogram(linear_spec_db,
-                 sampling_rate=sr,
-                 hop_length=hop_len,
-                 fmin=hparams.mel_fmin,
-                 fmax=hparams.mel_fmax,
-                 y_axis='linear',
-                 title='Linear-scale power spectrogram')
-
-# plot_spectrogram(mceps,
-#                  sampling_rate=sr,
-#                  hop_length=hop_len,
-#                  fmin=hparams.mel_fmin,
-#                  fmax=hparams.mel_fmax,
-#                  y_axis='linear',
-#                  title='Spectral envelope estimate from mel-cepstrum')
-
-# plot_spectrogram(mel_spec_db,
-#                  sampling_rate=sr,
-#                  hop_length=hop_len,
-#                  fmin=hparams.mel_fmin,
-#                  fmax=hparams.mel_fmax,
-#                  y_axis='mel',
-#                  title='Mel-scale power spectrogram')
-
-# # Calculate mfcc delta features.
-# mfccs_d1 = librosa.feature.delta(mfccs, width=3, order=1)
-# # Calculate mfcc delta-delta features.
-# mfccs_d2 = librosa.feature.delta(mfccs, width=3, order=2)
-#
-# features = np.vstack((mfccs, mfccs_d1, mfccs_d2))
-#
-# plot_feature_frames(mfccs, sampling_rate=sr, hop_length=hop_len, title='MFCC')
-# plot_feature_frames(mfccs_d1, sampling_rate=sr, hop_length=hop_len, title=r'MFCC-$\Delta$')
-# plot_feature_frames(mfccs_d2, sampling_rate=sr, hop_length=hop_len, title=r'MFCC-$\Delta^2$')
+# calculate_linear_spec(wav, hop_len, win_len)
+# calculate_mfccs_and_deltas(wav, hop_len, win_len)
+# resynth_wav_using_mcep(wav, hop_len, 25, 0.35)
