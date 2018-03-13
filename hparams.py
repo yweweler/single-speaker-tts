f7f1f9969ad9d6c88436346a4076173c13845e2a
Yves-Noel Weweler <y.weweler@gmail.com>
2018-03-13 11:33:52 +0100

    Documentation for the hyper-parameters

--- hparams.py
+++ hparams.py
@@ -2,12 +2,27 @@ import tensorflow as tf
 
 # Default hyper-parameters:
 hparams = tf.contrib.training.HParams(
+    # Target sampling rate.
     sampling_rate=16000,
+
+    # FFT window size.
     n_fft=512,
+
+    # Windows length in ms.
     win_len=25.0,
+
+    # Window stride in ms.
     win_hop=8.0,
+
+    # Number of Mel bands to generate.
     n_mels=128,
+
+    # Mel spectrum lower cutoff frequency.
     mel_fmin=0,
+
+    # Mel spectrum upper cutoff frequency.
     mel_fmax=8000,
+
+    # Number of Mel-frequency cepstral coefficients to generate.
     n_mfcc=13
 )
