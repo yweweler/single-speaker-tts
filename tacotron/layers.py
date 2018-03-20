a69bf981b6d5a87f62fbb0d30f2ccd8b7082a16d
Yves-Noel Weweler <y.weweler@gmail.com>
2018-03-20 14:07:34 +0100

    Implement the paper "Highway Networks"
    * Read these papers again:
        - [1]: Highway Networks, http://arxiv.org/abs/1505.00387
        - [2]: http://arxiv.org/abs/1502.01852
    
    * Added information from the Tacotron paper that mentions the used configuration.

--- tacotron/layers.py
+++ tacotron/layers.py
@@ -1,27 +1,55 @@
 import tensorflow as tf
 
 
-def highway_network(inputs, units, activation=None, t_bias_init=-1.0):
+def highway_network_layer(inputs, units, activation=tf.nn.relu, t_bias_init=-1.0):
     """
+    Implementation of a Highway Network layer.
+
+    See: http://arxiv.org/abs/1505.00387
 
     Arguments:
-        input:
-        units:
+        inputs (tf.Tensor):
+            The shape is expected to be shape=(B, T, F) with B being the batch size, T being the
+            number of time frames and F being the size of the features.
+
+        units (int):
+            The number of units in the highway layer.
+            The number of units is expected to match the feature size F.
+
+        activation (:obj:`function`, optional):
+            pass
+
+        t_bias_init (:obj:`float`, optional):
+            pass
 
     Returns:
+        tf.Tensor:
+            pass
 
     Notes:
-        - See: http://arxiv.org/abs/1505.00387
-        - We refer to T as the transform gate.
-        - We refer to C as the carry gate, setting C = 1-T.
-    """
-    # TODO: Assert inputs.dim == outputs.dim == H(inputs) == T(inputs)
+        - See [1]: Highway Networks, http://arxiv.org/abs/1505.00387
+        - See [2]: http://arxiv.org/abs/1502.01852
 
-    # Initialization of kernel and bias are strongly dependant on the functional form of H.
-    # In the paper they tested with activation functions ReLU and tanh.
-    # They used transform gate biases between -1 and -10.
-    # TODO: All other weights were initialized following the scheme introduced by (He et al., 2015).
-    # TODO: Does the tacotron paper state anything on their exact configuration?
+        The Tacotron paper only states that they are able to train their architecture using
+        "random initialization". However, it is unclear to me if they are using a plain Xavier
+        initialization or initialization based on (He et al., 2015) [2] as described in the
+        Highway Networks paper.
+
+        The procedure from [2] takes into account that the ReLU and PReLU activations are not
+        linear, as opposed to Xavier which assumes that activations are linear.
+        However, considering the measurements described in [2] there seems to be no superior
+        solution when using smaller networks instead of very deep networks.
+
+        As described in [1], section "2.2. Training Deep Highway Networks", a negative transform
+        gate bias initialization was sufficient for learning to proceed in very deep networks for
+        various zero-mean initial distributions of W_H and different activation functions used by
+        H.
+
+        Since Xavier initialization is zero-mean and the Highway Networks used in Tacotron are
+        not particularly deep there should not be a major impact from using it.
+    """
+    # Make sure the the input dimensionality is equal to the output dimensionality.
+    tf.assert_equal(inputs.shape[-1], units)
 
     h = tf.layers.dense(inputs=inputs,
                         units=units,
@@ -30,18 +58,14 @@ def highway_network(inputs, units, activation=None, t_bias_init=-1.0):
                         bias_initializer=tf.zeros_initializer(),
                         name='H')
 
+    # For the transform gate we follow [1], section "2.2 Training Deep Highway Networks" using a
+    # sigmoid activation function and a negative bias initializer.
     t = tf.layers.dense(inputs=inputs,
                         units=units,
-                        activation=tf.nn.sigmoid,  # See: "2.2 Training Deep Highway Networks"
-                        # kernel_initializer=tf.glorot_normal_initializer(),
+                        activation=tf.nn.sigmoid,
+                        kernel_initializer=tf.glorot_normal_initializer(),
                         bias_initializer=tf.constant_initializer(t_bias_init),
-                        # See: "2.2 Training Deep Highway Networks"
                         name='T')
 
-    # t_bias_init = -1.0
-    # ------------------------------------------------------------------------------------------------------------------
-    # They found that a negative bias initialization was sufficient for learning to proceed in very deep networks for
-    # various zero-mean initial distributions of W_H and different activation functions used by H.
-    # (See: "2.2. Training Deep Highway Networks")
-
+    # TODO: For some reason pycharm thinks that this is a plain floating point operation.
     return (h * t) + (inputs * (1.0 - t))
