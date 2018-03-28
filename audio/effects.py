587cb806f4f39bb480b8d0977a192b1cd0cea195
Yves-Noel Weweler <y.weweler@gmail.com>
2018-03-28 10:52:04 +0200

    Implement my own version of pitch shifting
    I have implemented my own version of the pitch shifting algorithm used in librosa that is based on my improved version of time shifting. My implementation gives considerably higher quality results.

--- audio/effects.py
+++ audio/effects.py
@@ -31,6 +31,42 @@ def pitch_shift(wav, sampling_rate, octaves):
     n_bins = 12
     return librosa.effects.pitch_shift(wav, sampling_rate, n_bins * octaves, n_bins)
 
+
+def pitch_shift_adv(wav, sampling_rate, octaves):
+    """
+    Pitch-shift a waveform by `octaves` octaves.
+
+    Arguments:
+        wav (np.ndarray):
+            Audio time series to pitch shift.
+            The shape is expected to be shape=(n,) for an mono waveform.
+
+        sampling_rate (:obj:`int`, optional):
+            Target sampling rate. When None is used, the sampling rate inferred from the file is used.
+            Defaults to None.
+
+        octaves (float):
+            Octaves to shift the pitch up or down.
+            Each octave is divided into 12 half-steps.
+            Therefore to shift one half-step one can use `octaves=1/12`.
+
+    Returns:
+        (np.ndarray):
+            Audio time series.
+            The shape of the returned array is shape=(n,) and the arrays dtype is np.float32.
+    """
+    rate = 2.0 ** (-octaves)
+
+    # Stretch time.
+    streched = time_stretch(wav, rate)
+
+    # Resample the signal.
+    y_shift = librosa.core.resample(streched, float(sampling_rate) / rate, sampling_rate)
+
+    # Crop to the same dimension as the input.
+    return librosa.util.fix_length(y_shift, len(wav))
+
+
 def time_stretch(wav, rate):
     """
     Time-stretch an audio series by a fixed rate.
