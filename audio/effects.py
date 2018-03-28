c5e6585a98358c1267d3c697a13aff60114dca2c
Yves-Noel Weweler <y.weweler@gmail.com>
2018-03-28 10:23:23 +0200

    Implement time stretching
    * Implement a time stretching helper.
    * Fix documentation.

--- audio/effects.py
+++ audio/effects.py
@@ -7,7 +7,7 @@ def pitch_shift(wav, sampling_rate, octaves):
 
     Arguments:
         wav (np.ndarray):
-            Audio time series to save.
+            Audio time series to pitch shift.
             The shape is expected to be shape=(n,) for an mono waveform.
 
         sampling_rate (:obj:`int`, optional):
@@ -20,13 +20,34 @@ def pitch_shift(wav, sampling_rate, octaves):
             Therefore to shift one half-step one can use `octaves=1/12`.
 
     Returns:
-
+        (np.ndarray):
+            Audio time series.
+            The shape of the returned array is shape=(n,) and the arrays dtype is np.float32.
     """
     n_bins = 12
     return librosa.effects.pitch_shift(wav, sampling_rate, n_bins * octaves, n_bins)
 
 
-def time_stretch(wav, sampling_rate, rate):
-    # TODO: Not implemented yet!
-    # librosa.effects.time_stretch()
-    pass
+def time_stretch(wav, rate):
+    """
+    Time-stretch an audio series by a fixed rate.
+
+    Arguments:
+        wav (np.ndarray):
+            Audio time series to time stretch.
+            The shape is expected to be shape=(n,) for an mono waveform.
+
+        rate (float):
+            Factor used to stretch the signal. `rate` is required to be > 0.
+            With `rate` > 1.0 the signal is speed up.
+            With 0.0 < `rate` < 1.0 the signal is slowed down.
+
+    Returns:
+        (np.ndarray):
+            Audio time series.
+            The shape of the returned array is shape=(n,) and the arrays dtype is np.float32.
+    """
+    if rate <= 0.0:
+        raise ValueError('The fixed rate used to stretch the signal must be greater 0.')
+
+    return librosa.effects.time_stretch(wav, rate)
