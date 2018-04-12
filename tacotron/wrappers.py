9d56f4e686150ae5657d3eb191ffb8cea8761d99
Yves-Noel Weweler <y.weweler@gmail.com>
2018-04-12 12:30:24 +0200

    Code cleanup
    * Cleaned up tacotron/wrappers.py

--- tacotron/wrappers.py
+++ tacotron/wrappers.py
@@ -5,7 +5,30 @@ from tacotron.layers import pre_net
 
 
 class PrenetWrapper(tfc.rnn.RNNCell):
+    """
+    RNNCell wrapper that applies a Pre-Net to the RNNs inputs as described in
+    "Tacotron: Towards End-to-End Speech Synthesis".
+
+    See: "Tacotron: Towards End-to-End Speech Synthesis"
+      * Source: [1] https://arxiv.org/abs/1703.10135
+    """
+
     def __init__(self, cell, pre_net_layers, training):
+        """
+        Wraps and RNNCell instance and applies a Pre-Net to the inputs.
+
+        Arguments:
+            cell (tensorflow.contrib.rnn.RNNCell):
+                RNN cell to be wrapped.
+
+            pre_net_layers (:obj:`list` of :obj:`tuple`):
+                A list of length L containing tuples of the form (units, dropout, activation )
+                defining the number of units the dropout rate and the activation function for L
+                layers.
+
+            training (boolean):
+                Boolean defining whether to apply the dropout or not.
+        """
         super(PrenetWrapper, self).__init__()
         self._cell = cell
         self._layers = pre_net_layers
@@ -13,20 +36,89 @@ class PrenetWrapper(tfc.rnn.RNNCell):
 
     @property
     def state_size(self):
+        """
+        Get the size(s) of state(s) used by teh wrapped cell.
+
+        It can be represented by an Integer, a TensorShape or a tuple of Integers
+        or TensorShapes.
+        Returns:
+            object:
+                It can be represented by an Integer, a TensorShape or a tuple of Integers
+                or TensorShapes.
+        """
         return self._cell.state_size
 
     @property
     def output_size(self):
+        """
+        Get the size of the outputs produced by the wrapped cell.
+
+        Returns:
+            object:
+                Integer or TensorShape.
+        """
         return self._cell.output_size
 
     def compute_output_shape(self, input_shape):
-        raise NotImplementedError()
+        """
+        Computes the output shape of the layer given the input shape.
+
+        Note:
+            - This is function is implemented since it is abstract in the super classes.
+            - This function is unused however.
+
+        Arguments:
+            input_shape: Unused
+        """
+        raise NotImplementedError
 
     def zero_state(self, batch_size, dtype):
+        """Return zero-filled state tensor(s).
+
+        Arguments:
+          batch_size (object):
+            int, float, or unit Tensor representing the batch size.
+
+          dtype (tf.DType):
+            The data type to use for the state.
+
+        Returns:
+            tf.Tensor:
+                - If `state_size` is an int or TensorShape, then the return value is a `N-D` tensor
+                of shape `[batch_size, state_size]` filled with zeros.
+                - If `state_size` is a nested list or tuple, then the return value is a nested
+                list or tuple (of the same structure) of `2-D` tensors with the shapes
+                `[batch_size, s]` for each s in `state_size`.
+
+        """
+
         return self._cell.zero_state(batch_size, dtype)
 
-    # TODO: Do we have to override the RNNLayer.call func. or the RNNCell.__call__ func. ?
-    def call(self, inputs, state, **kwargs):
+    def __call__(self, inputs, state, scope=None):
+        """
+        Run this RNN cell on inputs, starting from the given state.
+
+        Arguments:
+            inputs (tf.Tensor):
+                `2-D` tensor with shape `[batch_size, input_size]`.
+
+            state:
+                If `self.state_size` is an integer, this should be a `2-D Tensor` with shape
+                `[batch_size, self.state_size]`.  Otherwise, if `self.state_size` is a tuple of
+                integers, this should be a tuple with shapes `[batch_size, s] for s in
+                self.state_size`.
+
+            scope:
+                Unused.
+
+        Returns:
+            (output, new_state):
+                output (tf.Tensor):
+                    A `2-D` tensor with shape `[batch_size, self.output_size]`.
+                new_state (tf.Tensor):
+                    Either a single `2-D` tensor, or a tuple of tensors matching the arity and
+                    shapes of `state`.
+        """
         projected = pre_net(inputs, self._layers, scope='pre_net', training=self._training)
 
         return self._cell(projected, state)
