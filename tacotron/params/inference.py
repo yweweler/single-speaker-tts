7c5e236e011ed740dce529c20e698cc7b59b6edb
Yves-Noel Weweler <y.weweler@gmail.com>
2018-08-08 18:08:55 +0200

    Add support for dumping the linear spectrogram during inference

--- tacotron/params/inference.py
+++ tacotron/params/inference.py
@@ -23,8 +23,12 @@ inference_params = tf.contrib.training.HParams(
     synthesis_file='/tmp/inference/sentences.txt',
 
     # Flag controlling if the alignments should be dumped as .npz files.
-    # Dumps are written into `synthesis_dir`.
+    # Dumps are written into `synthesis_dir`/alignments.npz.
     dump_alignments=True,
+
+    # Flag controlling if the linear-scale spectrogram should be dumped as .npz files.
+    # Dumps are written into `synthesis_dir`/linear-spectrogram.npz.
+    dump_linear_spectrogram=True,
 
     # The number of process to threads to use for parallel Griffin-Lim reconstruction.
     n_synthesis_threads=6
