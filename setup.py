5d37b16e7d1b5b3daab2220ad678841474cce80a
Yves-Noel Weweler <y.weweler@gmail.com>
2018-10-16 16:52:43 +0200

    Update infos in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 
 def get_version():
-    path = os.path.join(os.path.dirname(__file__), 'conversion/__init__.py')
+    path = os.path.join(os.path.dirname(__file__), 'tacotron/__init__.py')
     with open(path) as file:
         for line in file:
             if line.startswith('__version__'):
@@ -19,24 +19,24 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 long_description = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 
 setup(
-    name='voice-conversion',
+    name='Single-Speaker End-to-End Neural Text-to-Speech Synthesis',
     version=get_version(),
     packages=find_packages(exclude=('tests', 'docs')),
 
     # PyPI metadata
     author='Yves-Noel Weweler',
     author_email='y.weweler@fh-muenster.de',
-    description='This module implements deep learning based voice conversion.',
+    description='This module implements a single-speaker neural text-to-speech (TTS) system capable of training in a end-to-end fashion.',
     long_description=long_description,
     license='MIT License',
-    keywords='mxnet audio autoencoder',
+    keywords='tensorflow audio neural tts end-to-end single-speaker',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Operating System :: OS Independent',
+        'Operating System :: Linux',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
--- tacotron/__init__.py
+++ tacotron/__init__.py
@@ -6,3 +6,4 @@ This module implements the Tacotron architecture using tensorflow.
 
 __author__ = 'Yves-Noel Weweler <y.weweler@fh-muenster.de>'
 __status__ = 'Development'
+__version__ = '0.1.1'
